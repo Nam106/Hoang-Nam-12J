9c163dc7578dda80413d9e4d0a042bc137af91dc
Nam106 <hoainamtruong42@gmail.com>
2025-02-26 10:23:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u = username.value
   let p = pw.value
-  if (u=="username" && p=="password")
+  if (u=="Nam" && p=="1062007")
   {
     window.open("DanhMucBT.html")
     document.getElementById(compiler).style.display="block"
